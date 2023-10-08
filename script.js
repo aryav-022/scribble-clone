0f054a0bd5640b1761111a75678faffb302aafa1
Aryav <aryavbhola@gmail.com>
2023-10-08 15:55:33 +0530

    sidebar added

--- script.js
+++ script.js
@@ -6,21 +6,26 @@ slate = new Slate(canvas);
 const keyMap = ["Pan", "Pencil", "Eraser"];
 
 function changeTool(key) {
-    slate.toolSelected = keyMap[key - 1];
-    
-    const tools = document.querySelectorAll(".tool");
-    
-    tools.forEach((tool) => {
-        if (tool.getAttribute('data-tool') === keyMap[key - 1]) {
-            tool.classList.add('active');
-        } else {
-            tool.classList.remove('active');
-        }
-    })
+	slate.toolSelected = keyMap[key - 1];
+
+	const tools = document.querySelectorAll(".tool");
+
+	tools.forEach((tool) => {
+		if (tool.getAttribute("data-tool") === keyMap[key - 1]) {
+			tool.classList.add("active");
+		} else {
+			tool.classList.remove("active");
+		}
+	});
 }
 
 window.addEventListener("keypress", (e) => {
-    const key = e.key;
-    if (key < "1" || key > "2") return;
-    changeTool(parseInt(key));
-})
+	const key = e.key;
+	if (key < "1" || key > "2") return;
+	changeTool(parseInt(key));
+});
+
+function toggleSidebar() {
+	const sidebar = document.querySelector(".sidebar");
+	sidebar.classList.toggle("hidden");
+}
